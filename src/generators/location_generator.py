e8727ca3cf03684e57a23fc50dac95d8365cdbeb
unalivij <jed.unalivia@lexisnexisrisk.com>
2026-08-10 19:46:47 +0800

    Added new generator location_generator.py

---
+++ src/generators/location_generator.py
@@ -0,0 +1,22 @@
+# Fetch random location data from locations.json and return a dictionary of the location data
+import json
+import random
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+LOCATION_FILE = BASE_DIR / 'locations.json'
+
+with open(LOCATION_FILE) as f:
+    LOCATIONS = json.load(f)
+
+def get_random_location() -> dict:
+    """
+    Returns a dictionary representing a randomly chosen location.
+    return: A dictionary containing the location data.
+    """
+    return random.choice(LOCATIONS)
+
+
+# Test the function
+# if __name__ == "__main__":
+#     print(get_random_location())
